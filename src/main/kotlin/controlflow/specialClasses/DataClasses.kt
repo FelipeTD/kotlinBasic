4eccb7e768774d887fd68115fb68adc3b935528f
filipe.dias <fedispato@gmail.com>
2024-01-07 16:12:06 -0300

    Adicionando exemplos de classes especiais no kotlin

---
+++ src/main/kotlin/controlflow/specialClasses/DataClasses.kt
@@ -0,0 +1,35 @@
+package controlflow.specialClasses
+
+data class User(val name: String, val id: Int) {
+    override fun equals(other: Any?) =
+        other is User && other.id == this.id
+}
+
+fun main() {
+
+    val user = User("Alex", 1)
+    println(user)
+
+    val secondUser = User("Alex", 1)
+    val thirdUser = User("Max", 2)
+
+    println("user == secondUser: ${user == secondUser}")
+    println("user == thirdUser: ${user == thirdUser}")
+
+    // hashCode() function
+    println(user.hashCode())
+    println(secondUser.hashCode())
+    println(thirdUser.hashCode())
+    println()
+
+    // copy() function
+    println(user.copy())
+    println(user === user.copy())
+    println(user.copy("Max"))
+    println(user.copy(id = 3))
+    println()
+
+    println("name = ${user.component1()}")
+    println("id = ${user.component2()}")
+
+}
